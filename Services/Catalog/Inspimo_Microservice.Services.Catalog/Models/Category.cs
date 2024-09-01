5159650e37db88db3ca429c50f53f1968c18fe4f
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-09-01 16:35:34 +0300

    Microservis de Catalog servisi için projeyi oluşturdum. Enttiy ler tanımlandı.

---
+++ Services/Catalog/Inspimo_Microservice.Services.Catalog/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Inspimo_Microservice.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]//Hangi sütunun id olduğunu belirtiyor.
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+        //MongoDb nin ID değeri string
+        public string CategoryName { get; set; }
+    }
+}
