c7dbaaa41e25c20863968f75e8f710c8cb0a3b24
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-09-10 21:55:06 +0300

    Category ve Product servislerini yazdık. startup cs de gerekli konfigürasyon işlemleri yapıldı. api yı ayağa kaldırmak için controller ları ekledik.

---
+++ Services/Catalog/Inspimo_Microservice.Services.Catalog/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Inspimo_Microservice.Services.Catalog.Dtos;
+using Inspimo_Microservice.Services.Catalog.Services.Abstract;
+using Inspimo_Microservice.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Inspimo_Microservice.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : CustomBaseController
+    {
+        private readonly ICategoroyService _categoroyService;
+
+        public CategoryController(ICategoroyService categoroyService)
+        {
+            _categoroyService = categoroyService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var values = await _categoroyService.GetAllAsync();
+            return CreateActionResultInstance(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(CreateCategoryDto createCategoryDto)
+        {
+            var value = await _categoroyService.CreateAsync(createCategoryDto);
+            return CreateActionResultInstance(value);
+        }
+    }
+}
--- Services/Catalog/Inspimo_Microservice.Services.Catalog/Startup.cs
+++ Services/Catalog/Inspimo_Microservice.Services.Catalog/Startup.cs
@@ -1,3 +1,8 @@
+using AutoMapper;
+using Inspimo_Microservice.Services.Catalog.Services.Abstract;
+using Inspimo_Microservice.Services.Catalog.Services.Concrete;
+using Inspimo_Microservice.Services.Catalog.Settings.Abstract;
+using Inspimo_Microservice.Services.Catalog.Settings.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -26,7 +32,14 @@ namespace Inspimo_Microservice.Services.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<ICategoroyService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddAutoMapper(typeof(Startup));
+            services.Configure<DataBaseSettings>(Configuration.GetSection("DataBaseSettings"));
+            services.AddSingleton<IDataBaseSettings>(sp =>
+            {
+                return sp.GetRequiredService<IOptions<DataBaseSettings>>().Value;
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
